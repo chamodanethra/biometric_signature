d3d815f49d8954aa1e1b1951ecfdcec5d0ce8cda
chamodanethra <chamodananayakkara@gmail.com>
2023-01-28 20:32:45 +0530

    fixing bugs

--- ios/Classes/BiometricSignaturePlugin.swift
+++ ios/Classes/BiometricSignaturePlugin.swift
@@ -3,60 +3,60 @@ import UIKit
 import LocalAuthentication
 import Security
 
+@available(iOS 11.3, *)
 public class BiometricSignaturePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "biometric_signature", binaryMessenger: registrar.messenger())
-    let instance = BiometricSignaturePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-      switch call.method {
-          case "createKeys":
-              createKeys(result)
-          case "createSignature":
-              createSignature(call.arguments, result)
-          case "deleteKeys":
-              result(FlutterMethodNotImplemented)
-          case "biometricAuthAvailable":
-              biometricAuthAvailable(result)
-          case "biometricKeysExist":
-              biometricKeysExist(result)
-          default:
-              result(FlutterMethodNotImplemented)
-      }
-  }
-
-  private func biometricAuthAvailable(result: @escaping FlutterResult) -> Void  {
-          let context = LAContext()
-          var la_error: NSError?
-          let canEvaluatePolicy = context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &la_error)
-
-          var resultMap = [String : String]()
-          if canEvaluatePolicy {
-              let biometryType = getBiometryType(context)
-              resultMap = [
-                  "biometricsType": biometryType
-              ]
-          } else {
-              var errorMessage: String? = nil
-              if let la_error {
-                  errorMessage = "\(la_error)"
-              }
-              resultMap = [
-                  "biometricsType": "none",
-                  "error": errorMessage ?? ""
-              ]
-          }
-          result(resultMap)
-          return
-      }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "biometric_signature", binaryMessenger: registrar.messenger())
+        let instance = BiometricSignaturePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
     
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "createKeys":
+            self.createKeys(result: result)
+        case "createSignature":
+            self.createSignature(options: call.arguments as? Dictionary<String, String>, result: result)
+        case "deleteKeys":
+            self.deleteKeys(result: result)
+        case "biometricAuthAvailable":
+            self.biometricAuthAvailable(result: result)
+        case "biometricKeysExist":
+            self.biometricKeysExist(result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
     
-    func biometricKeysExist(result: @escaping FlutterResult) -> Void{
-        DispatchQueue.global(qos: .default).async(execute: { [self] in
+    private func biometricAuthAvailable(result: @escaping FlutterResult) -> Void  {
+        let context = LAContext()
+        var la_error: NSError?
+        let canEvaluatePolicy = context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &la_error)
+        
+        var resultMap = [String : String]()
+        if canEvaluatePolicy {
+            let biometryType = getBiometryType(context)
+            resultMap = [
+                "biometricsType": biometryType
+            ]
+        } else {
+            var errorMessage: String? = nil
+            if let la_error {
+                errorMessage = "\(la_error)"
+            }
+            resultMap = [
+                "biometricsType": "none",
+                "error": errorMessage ?? ""
+            ]
+        }
+        result(resultMap)
+        return
+    }
+    
+    private func biometricKeysExist(result: @escaping FlutterResult) -> Void{
+        DispatchQueue.global().async(execute: { [self] in
             let biometricKeyExists = self.doesBiometricKeyExist()
-            if (biometricKeyExists) {
+            if biometricKeyExists {
                 let resultBoolean = true
                 result(resultBoolean)
             } else {
@@ -67,24 +67,19 @@ public class BiometricSignaturePlugin: NSObject, FlutterPlugin {
     }
     
     private func deleteKeys(result: @escaping FlutterResult) -> Void  {
-            let group = DispatchGroup()
-            DispatchQueue.global(qos: .default).async(execute: { [self] in
-                let biometricKeyExists = self.doesBiometricKeyExist()
-                if biometricKeyExists {
-                    self.deleteBiometricKey() { status in
-                        print(status)
-                        result(status == noErr)
-                        group.leave()
-                        group.wait()
-                    }
-                }
-            })
-        }
+        DispatchQueue.global().async(execute: { [self] in
+            let biometricKeyExists = self.doesBiometricKeyExist()
+            if biometricKeyExists {
+                var status = self.deleteBiometricKey()
+                print(status)
+                result(status == noErr)
+            }
+        })
+    }
     
     
     private func createKeys(result: @escaping FlutterResult) -> Void  {
-        let group = DispatchGroup()
-        DispatchQueue.global(qos: .default).async(execute: { [self] in
+        DispatchQueue.global().async(execute: { [self] in
             var error: Unmanaged<CFError>? = nil
             let sacObject = SecAccessControlCreateWithFlags(
                 kCFAllocatorDefault,
@@ -118,46 +113,43 @@ public class BiometricSignaturePlugin: NSObject, FlutterPlugin {
                         ] as? Any
                     ]
                 }
-                self.deleteBiometricKey() { status in
-                    print(status)
-                    group.leave()
-                    group.wait()
-                    var error: Unmanaged<CFError>?
-                    var privateKey: Any? = nil
-                    if let attributes = keyAttributes as? CFDictionary? {
-                        privateKey = SecKeyCreateRandomKey(attributes!, &error)
-                        if let privateKey {
-                            var publicKey: Any? = nil
-                            if let key = privateKey as? SecKey? {
-                                publicKey = SecKeyCopyPublicKey(key!)
-                            }
-                            var publicKeyDataRef: CFData? = nil
-                            if let key = publicKey {
-                                publicKeyDataRef = SecKeyCopyExternalRepresentation(key as! SecKey, nil)
-                            }
-                            let publicKeyData = publicKeyDataRef as? Data
-                            let publicKeyDataWithHeader = BiometricSignaturePlugin.addHeaderPublickey(publicKeyData)
-                            let publicKeyString = publicKeyDataWithHeader?.base64EncodedString(options: [])
-                            
-                            let resultMap = [
-                                "publicKey": publicKeyString ?? ""
-                            ]
-                            result(resultMap)
-                        } else {
-                            let platformError = FlutterError(code: "AUTHFAILED",
-                                                             message: "Key generation error: \(error)",
-                                                             details: nil)
-                            result(platformError)
+                var status = self.deleteBiometricKey()
+                print(status)
+                var error: Unmanaged<CFError>?
+                var privateKey: Any? = nil
+                if let attributes = keyAttributes as? CFDictionary? {
+                    privateKey = SecKeyCreateRandomKey(attributes!, &error)
+                    if let privateKey {
+                        var publicKey: Any? = nil
+                        if let key = privateKey as? SecKey? {
+                            publicKey = SecKeyCopyPublicKey(key!)
                         }
-                        return
+                        var publicKeyDataRef: CFData? = nil
+                        if let key = publicKey {
+                            publicKeyDataRef = SecKeyCopyExternalRepresentation(key as! SecKey, nil)
+                        }
+                        let publicKeyData = publicKeyDataRef as? Data
+                        let publicKeyDataWithHeader = BiometricSignaturePlugin.addHeaderPublickey(publicKeyData)
+                        let publicKeyString = publicKeyDataWithHeader?.base64EncodedString(options: [])
+                        
+                        let resultMap = [
+                            "publicKey": publicKeyString ?? ""
+                        ]
+                        result(resultMap)
+                    } else {
+                        let platformError = FlutterError(code: "AUTHFAILED",
+                                                         message: "Key generation error: \(error)",
+                                                         details: nil)
+                        result(platformError)
                     }
+                    return
                 }
             }
         })
     }
     
     private func createSignature(options: Dictionary<String, String>?, result: @escaping FlutterResult) -> Void  {
-        DispatchQueue.global(qos: .default).async(execute: { [self] in
+        DispatchQueue.global().async(execute: { [self] in
             let promptMessage = options?["promptMessage"] ?? "Welcome"
             let payload = "arhten adomahc"
             let biometricKeyTag = getBiometricKey()
@@ -215,7 +207,7 @@ public class BiometricSignaturePlugin: NSObject, FlutterPlugin {
         })
     }
     
-    private func deleteBiometricKey(completion: @escaping (OSStatus) -> Void) {
+    private func deleteBiometricKey() -> OSStatus {
             let biometricKeyTag = getBiometricKey()
             var deleteQuery: [AnyHashable? : Any?]? = nil
             if let kSecClass = kSecClass as? AnyHashable, let kSecClassKey = kSecClassKey as? AnyHashable, let kSecAttrApplicationTag = kSecAttrApplicationTag as? AnyHashable, let kSecAttrKeyType = kSecAttrKeyType as? AnyHashable, let kSecAttrKeyTypeRSA = kSecAttrKeyTypeRSA as? AnyHashable {
@@ -228,8 +220,8 @@ public class BiometricSignaturePlugin: NSObject, FlutterPlugin {
             var status: OSStatus? = nil
             if let query = deleteQuery as? CFDictionary? {
                 status = SecItemDelete(query!)
-                completion(status!)
             }
+            return status!
         }
     
     private func getBiometryType(_ context: LAContext?) -> String {
@@ -242,8 +234,8 @@ public class BiometricSignaturePlugin: NSObject, FlutterPlugin {
         return biometricKeyTag
     }
     
-    func doesBiometricKeyExist() -> Bool {
-        let biometricKeyTag = getBiometricKeyTag()
+    private func doesBiometricKeyExist() -> Bool {
+        let biometricKeyTag = getBiometricKey()
         let searchQuery: [String: Any] = [
             kSecClass as String: kSecClassKey,
             kSecAttrApplicationTag as String: biometricKeyTag,
@@ -254,68 +246,53 @@ public class BiometricSignaturePlugin: NSObject, FlutterPlugin {
         let status = SecItemCopyMatching(searchQuery as CFDictionary, &item)
         return status == errSecSuccess
     }
-
-      static let addHeaderPublickey_encodedRSAEncryptionOID:[UInt8] = [    // Sequence of length 0xd made up of OID followed by NULL
-          0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00]
-
-      static func addHeaderPublickey(_ publicKeyData: Data?) -> Data? {
-
-          var builder = [UInt8](repeating: 0, count: 15)
-          var encKey = Data()
-          var bitstringEncLength: UInt
-          // When we get to the bitstring - how will we encode it?
-          if (publicKeyData?.count ?? 0) + 1 < 128 {
-              bitstringEncLength = 1
-          } else {
-              bitstringEncLength = UInt((((publicKeyData?.count ?? 0) + 1) / 256) + 2)
-          }
-          //
-          //        // Overall we have a sequence of a certain length
-          builder[0] = 0x30 // ASN.1 encoding representing a SEQUENCE
-          //        // Build up overall size made up of -
-          //        // size of OID + size of bitstring encoding + size of actual key
-          let i = MemoryLayout.size(ofValue: addHeaderPublickey_encodedRSAEncryptionOID) + 2 + Int(bitstringEncLength) + (publicKeyData?.count ?? 0)
-          var j = encodeLength(&builder[1], i)
-          encKey.append(&builder, count: Int(j + 1))
-
-          // First part of the sequence is the OID
-          encKey.append(
-              addHeaderPublickey_encodedRSAEncryptionOID,
-              count: MemoryLayout.size(ofValue: addHeaderPublickey_encodedRSAEncryptionOID))
-
-          // Now add the bitstring
-          builder[0] = 0x03
-          j = encodeLength(&builder[1], (publicKeyData?.count ?? 0) + 1)
-          builder[j + 1] = 0x00
-          encKey.append(&builder, count: Int(j + 2))
-
-          // Now the actual key
-          if let publicKeyData {
-              encKey.append(publicKeyData)
-          }
-
-          return encKey
-      }
-
-      static func encodeLength(_ buf: UnsafeMutablePointer<UInt8>?, _ length: size_t) -> size_t {
-          var length = length
-
-          // encode length in ASN.1 DER format
-          if length < 128 {
-              buf?[0] = UInt8(length)
-              return 1
-          }
-
-          let i: size_t = Int((length / 256)) + 1
-          buf?[0] = UInt8(i + 0x80)
-          for j in 0..<i {
-              buf?[i - j] = UInt8(length & 0xff)
-              length = size_t(length >> 8)
-          }
-
-          return size_t(i + 1)
-      }
-      func keychainError(error:  OSStatus) -> String {
-          return String(format: "%ld", Int(error))
-      }
+    
+    private static let addHeaderPublickey_encodedRSAEncryptionOID:[UInt8] = [
+        0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00]
+    
+    private static func addHeaderPublickey(_ publicKeyData: Data?) -> Data? {
+        var builder = [UInt8](repeating: 0, count: 15)
+        var encKey = Data()
+        var bitstringEncLength: UInt
+        if (publicKeyData?.count ?? 0) + 1 < 128 {
+            bitstringEncLength = 1
+        } else {
+            bitstringEncLength = UInt((((publicKeyData?.count ?? 0) + 1) / 256) + 2)
+        }
+        builder[0] = 0x30
+        let i = MemoryLayout.size(ofValue: addHeaderPublickey_encodedRSAEncryptionOID) + 2 + Int(bitstringEncLength) + (publicKeyData?.count ?? 0)
+        var j = encodedLength(&builder[1], i)
+        encKey.append(&builder, count: Int(j + 1))
+        
+        encKey.append(
+            addHeaderPublickey_encodedRSAEncryptionOID,
+            count: MemoryLayout.size(ofValue: addHeaderPublickey_encodedRSAEncryptionOID))
+        builder[0] = 0x03
+        j = encodedLength(&builder[1], (publicKeyData?.count ?? 0) + 1)
+        builder[j + 1] = 0x00
+        encKey.append(&builder, count: Int(j + 2))
+        if let publicKeyData {
+            encKey.append(publicKeyData)
+        }
+        return encKey
+    }
+    
+    private static func encodedLength(_ buf: UnsafeMutablePointer<UInt8>?, _ length: size_t) -> size_t {
+        var length = length
+        if length < 128 {
+            buf?[0] = UInt8(length)
+            return 1
+        }
+        let i: size_t = Int((length / 256)) + 1
+        buf?[0] = UInt8(i + 0x80)
+        for j in 0..<i {
+            buf?[i - j] = UInt8(length & 0xff)
+            length = size_t(length >> 8)
+        }
+        return size_t(i + 1)
+    }
+    
+    private func keychainError(error:  OSStatus) -> String {
+        return String(format: "%ld", Int(error))
+    }
 }
