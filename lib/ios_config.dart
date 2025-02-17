c3378bbf5eeb6a9a1a8acd168926fbad6110246e
Samo8 <samodubovec@gmail.com>
2025-02-17 14:02:40 +0100

    feat: updating iosConfig

--- lib/ios_config.dart
+++ lib/ios_config.dart
@@ -1,16 +1,19 @@
 class IosConfig {
   bool useDeviceCredentials;
   bool enforceBiometric;
+  Map<String?, String?>? options;
 
   IosConfig({
     required this.useDeviceCredentials,
     this.enforceBiometric = false,
+    this.options,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'useDeviceCredentials': useDeviceCredentials,
       'enforceBiometric': enforceBiometric,
+      'options': options,
     };
   }
 }
